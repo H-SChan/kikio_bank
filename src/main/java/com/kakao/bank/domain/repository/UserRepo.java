d6379a0fe3b1e5aabd7cc62a79df84308ec05fee
H-SChan <dlwp7581@gmail.com>
2021-09-25 03:05:08 +0900

    [add] query

--- src/main/java/com/kakao/bank/domain/repository/UserRepo.java
+++ src/main/java/com/kakao/bank/domain/repository/UserRepo.java
@@ -3,5 +3,8 @@ package com.kakao.bank.domain.repository;
 import com.kakao.bank.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepo extends JpaRepository<User, Long> {
+    Optional<User> findById(String id);
 }
