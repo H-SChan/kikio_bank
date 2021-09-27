adc5e536d728da97d461896b733d22e3b2d802d9
H-SChan <dlwp7581@gmail.com>
2021-09-27 23:21:28 +0900

    [add] constructor

--- src/main/java/com/kakao/bank/domain/entity/User.java
+++ src/main/java/com/kakao/bank/domain/entity/User.java
@@ -1,5 +1,6 @@
 package com.kakao.bank.domain.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,7 @@ import javax.persistence.*;
 
 @Builder
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Entity
 public class User {
