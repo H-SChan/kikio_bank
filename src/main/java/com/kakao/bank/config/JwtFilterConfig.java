11aaab21c786d765bf1b60e6e9175d7cfe59087e
H-SChan <dlwp7581@gmail.com>
2021-10-07 23:08:13 +0900

    [fix] jwt filter config

---
+++ src/main/java/com/kakao/bank/config/JwtFilterConfig.java
@@ -0,0 +1,28 @@
+package com.kakao.bank.config;
+
+import com.kakao.bank.filter.JwtAuthenticationFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+@Configuration
+public class JwtFilterConfig {
+
+    private final HandlerExceptionResolver handlerExceptionResolver;
+
+    public JwtFilterConfig(HandlerExceptionResolver handlerExceptionResolver) {
+        this.handlerExceptionResolver = handlerExceptionResolver;
+    }
+
+    @Bean
+    public FilterRegistrationBean<JwtAuthenticationFilter> authFilter() {
+        FilterRegistrationBean<JwtAuthenticationFilter> registrationBean = new FilterRegistrationBean<>();
+
+        registrationBean.setFilter(new JwtAuthenticationFilter(handlerExceptionResolver));
+        registrationBean.addUrlPatterns("/user/*");
+        registrationBean.setOrder(2);
+
+        return registrationBean;
+    }
+}
