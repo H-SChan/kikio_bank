f34f5952361f3d1ff878ffecf5db6cfc6d75895c
H-SChan <dlwp7581@gmail.com>
2021-10-14 09:57:33 +0900

    [update] kind of bank

--- src/main/java/com/kakao/bank/domain/enums/Bank.java
+++ src/main/java/com/kakao/bank/domain/enums/Bank.java
@@ -1,4 +1,12 @@
 package com.kakao.bank.domain.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Bank {
+    SAECHAN("108");
+
+    private final String bankNum;
 }
