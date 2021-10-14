cb0073f4a4b325775b2d92224a1b4b2d742415fc
H-SChan <dlwp7581@gmail.com>
2021-10-14 23:04:52 +0900

    [fix] regexp

--- src/main/java/com/kakao/bank/domain/dto/auth/request/RegisterReqDto.java
+++ src/main/java/com/kakao/bank/domain/dto/auth/request/RegisterReqDto.java
@@ -2,19 +2,18 @@ package com.kakao.bank.domain.dto.auth.request;
 
 import com.kakao.bank.domain.entity.User;
 import lombok.Getter;
+import lombok.Setter;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Getter @Setter
 public class RegisterReqDto {
     @Pattern(regexp = "^[a-zA-Z0-9]*$", message = "아이디는 영어와 숫자만 입력해주세요.")
-    @Min(value = 5, message = "id는 5글자 이상 입력해야합니다.")
+    @Size(min = 5, message = "id는 5글자 이상 입력해야합니다.")
     private String id;
 
-    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)$", message = "영어, 숫자, 특수문자를 각각 한번 이상 입력해주세요")
+    @Pattern(regexp = "^[A-Za-z[0-9]$@$!%*#?&]*$", message = "영어, 숫자, 특수문자를 각각 한번 이상 입력해주세요")
     @Size(min = 8, max = 16, message = "비밀번호는 8~16글자 사이로 입력해주세요.")
     private String password;
 
@@ -26,7 +25,9 @@ public class RegisterReqDto {
 
     private String nickname;
 
-    public User toEntity() {
+    private Integer simpleNumber;
+
+    public User toEntity(String img) {
         return User.builder()
                 .id(id)
                 .password(password)
@@ -34,6 +35,8 @@ public class RegisterReqDto {
                 .residentRegistrationNumber(residentRegistrationNumber)
                 .name(name)
                 .nickname(nickname)
+                .profileImg(img)
+                .simpleNumber(simpleNumber)
                 .build();
     }
 }
