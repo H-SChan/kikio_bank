c690949170e56ba2bfc1a8a47891b720fa1163bd
H-SChan <dlwp7581@gmail.com>
2021-10-26 17:29:04 +0900

    [update] enumName

--- src/main/java/com/kakao/bank/domain/enums/AccountType.java
+++ src/main/java/com/kakao/bank/domain/enums/AccountType.java
@@ -6,10 +6,12 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum AccountType {
-    BASIC("입출금", "10"),
-    DEPOSIT("예금", "20"),
-    INSTALLMENT_SAVING("적금", "30");
+    BASIC("BASIC", "입출금", "10"),
+    DEPOSIT("DEPOSIT", "예금", "20"),
+    INSTALLMENT_SAVING("INSTALLMENT_SAVING", "적금", "30");
 
+    // enum 이름
+    private final String enumName;
     // 종류
     private final String type;
     // 식별 번호
--- src/main/java/com/kakao/bank/domain/enums/Bank.java
+++ src/main/java/com/kakao/bank/domain/enums/Bank.java
@@ -6,13 +6,13 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum Bank {
-    KAKAO("108", "새찬"),
-    KBANK("110", "장우"),
-    TOSS("666", "승도"),
-    DAEGU("031", "승우"),
-    MAAGU("719", "지나");
+    KAKAO("KAKAO", "108", "새찬"),
+    KBANK("KBANK", "110", "장우"),
+    TOSS("TOSS", "666", "승도"),
+    DAEGU("DAEGU", "031", "승우"),
+    MAAGU("MAAGU", "719", "지나");
 
-
+    private final String enumName;
     private final String bankNum;
     private final String name;
 }
